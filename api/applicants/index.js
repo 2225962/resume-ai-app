ecee8c96d333af1e2583baf4fc2bd5fa52bfcf85
Josh <153058402+2225962@users.noreply.github.com>
2026-05-11 22:31:07 +0800

    Update index.js

--- api/applicants/index.js
+++ api/applicants/index.js
@@ -1,14 +1,17 @@
+let applicants = [];
+
 module.exports = async function (context, req) {
+
+    if (req.method === "POST") {
+        const newApplicant = req.body;
+        applicants.push(newApplicant);
+    }
+
     context.res = {
         status: 200,
         headers: {
             "Content-Type": "application/json"
         },
-        body: [
-            {
-                message: "API WORKING",
-                timestamp: new Date()
-            }
-        ]
+        body: applicants
     };
 };
