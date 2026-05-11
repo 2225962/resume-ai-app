6f64c644146e83071ce7d4345d2111adf83468c4
Josh <153058402+2225962@users.noreply.github.com>
2026-05-11 22:00:34 +0800

    Update index.js

--- api/applicants/index.js
+++ api/applicants/index.js
@@ -1,39 +1,8 @@
-const { TableClient } = require("@azure/data-tables");
-
 module.exports = async function (context, req) {
-
-    const client = TableClient.fromConnectionString(
-        process.env.AZURE_STORAGE_CONNECTION_STRING,
-        "Applicants"
-    );
-
-    // GET → fetch all applicants
-    if (req.method === "GET") {
-
-        let results = [];
-
-        for await (const entity of client.listEntities()) {
-            results.push(entity);
+    context.res = {
+        status: 200,
+        body: {
+            message: "API WORKING"
         }
-
-        context.res = {
-            body: results
-        };
-    }
-
-    // POST → save applicant
-    if (req.method === "POST") {
-
-        const data = req.body;
-
-        await client.createEntity({
-            partitionKey: "applicants",
-            rowKey: Date.now().toString(),
-            ...data
-        });
-
-        context.res = {
-            body: { message: "Saved" }
-        };
-    }
+    };
 };
